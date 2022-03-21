8e35ae8fe22d18c1899cb505595f5da7cab386a9
Lucas <lucas.orodrigues@hotmail.com>
2022-03-21 13:23:10 -0300

    Projeto_blg

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import'./Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src="https://coletivocentopeia.com.br/2019/wp-content/uploads/2021/04/Trabalhar-em-Equipe-scaled.jpg" alt="imagem Tela inicial" className='img'/>
+        </>
+    );
+}
+
+export default Home;
