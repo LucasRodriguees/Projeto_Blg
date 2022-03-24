4e9ef7c280a87e951d8c0468a07cf5ad961092da
Lucas <lucas.orodrigues@hotmail.com>
2022-03-24 16:58:51 -0300

    Introdução_react

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
-import Navbar from "./components/estaticos/navbar/Navbar"
-import Footer from './components/estaticos/footer/Footer';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from "./components/estaticos/navbar/Navbar";
+import Footer from "./components/estaticos/footer/Footer";
+import Login from './paginas/login/Login';
 import Home from './paginas/home/Home';
 import './App.css';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home />
+      <Switch>
+        <div style={{minHeight: "100vh"}}>
+          
+        <Route exact path="/">
+            <Login />
+          </Route>
+
+        <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/home">
+            <Home />
+          </Route>
+        </div>
+      </Switch>
       <Footer />
-    </>
+    </Router>
   );
 }
 
